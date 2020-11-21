6f0aaf7e97909a654a762c18f413048087c02c5f
qodhrkawk <gabriel1120@naver.com>
2020-11-22 07:43:06 +0900

    d

--- Marshmallow_iOS/Marshmallow_iOS/Sources/VCs/ResultVC.swift
+++ Marshmallow_iOS/Marshmallow_iOS/Sources/VCs/ResultVC.swift
@@ -10,12 +10,27 @@ import UIKit
 class ResultVC: UIViewController {
     @IBOutlet weak var resultTableView: UITableView!
     
+    var names : [String] = []
+    var percents : [Int] = []
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         resultTableView.dataSource = self
+        self.resultTableView.separatorStyle = .none
+        
         
         // Do any additional setup after loading the view.
     }
+    
+    
+    
+    @IBAction func xButtonAction(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    
 }
 
 extension ResultVC: UITableViewDataSource {
@@ -30,7 +45,7 @@ extension ResultVC: UITableViewDataSource {
         else if section == 1{
             return 1
         }
-        return 5
+        return names.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -42,7 +57,11 @@ extension ResultVC: UITableViewDataSource {
             let cell = tableView.dequeueReusableCell(withIdentifier: LabelCell.identifier, for: indexPath)
             return cell
         }
-        let cell = tableView.dequeueReusableCell(withIdentifier: PersonalResultCell.identifier, for: indexPath)
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: PersonalResultCell.identifier, for: indexPath) as? PersonalResultCell
+        else {
+            return UITableViewCell()
+        }
+        cell.setItems(userName: names[indexPath.item], per: percents[indexPath.item])
         return cell
     }
     
